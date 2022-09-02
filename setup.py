3ecc602f34d24974c33e784c6380688bd049571a
Jacob Tomlinson <jtomlinson@nvidia.com>
2022-09-02 14:20:41 +0100

    Remove black changes

--- setup.py
+++ setup.py
@@ -4,20 +4,20 @@ except ImportError:
     from distutils.core import setup
 
 setup(
-    name="k8s-crd-resolver",
-    version="0.10.0",
-    description="Kubernetes CRD Resolver",
-    url="http://github.com/elemental-lf/k8s-crd-resolver",
-    author="Lars Fenneberg",
-    author_email="lf@elemental.net",
-    license="Apache-2.0",
+    name='k8s-crd-resolver',
+    version='0.10.0',
+    description='Kubernetes CRD Resolver',
+    url='http://github.com/elemental-lf/k8s-crd-resolver',
+    author='Lars Fenneberg',
+    author_email='lf@elemental.net',
+    license='Apache-2.0',
     install_requires=[
-        "ruamel.yaml>0.17,<0.18",
-        "prance[osv]>=0.21.8.0",
-        "openapi-spec-validator<0.5.0",
-        "jsonpatch>=1.32,<2",
+     'ruamel.yaml>0.17,<0.18',
+     'prance[osv]>=0.21.8.0',
+     'openapi-spec-validator<0.5.0',
+     'jsonpatch>=1.32,<2'
     ],
-    packages=["k8s_crd_resolver"],
+    packages=['k8s_crd_resolver'],
     include_package_data=True,
     entry_points="""
         [console_scripts]
